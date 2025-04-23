3479fb50ca01e3bbf1553a9433ae49eb10c50746
Chris Twiner <chris.twiner@gmail.com>
2025-04-23 13:41:05 +0200

    #1 - only 3.5 versions possible for jdk support RC1 prep ~cut-release

--- src/main/scala/com/sparkutils/dmn/kogito/ResultProcessors.scala
+++ src/main/scala/com/sparkutils/dmn/kogito/ResultProcessors.scala
@@ -7,16 +7,18 @@ import org.apache.spark.sql.catalyst.expressions.{LeafExpression}
 import org.apache.spark.sql.catalyst.expressions.codegen.{CodegenContext, ExprCode}
 import org.apache.spark.sql.catalyst.util.GenericArrayData
 
+import scala.collection.JavaConverters._
+
 import java.util
 
 case class KogitoSeqOfBools() extends LeafExpression with SeqOfBools {
 
   override def process(dmnResult: dmn.DMNResult): Any = {
     val res = dmnResult.asInstanceOf[KogitoDMNResult].result
-    if (res.hasErrors || res.getDecisionResults.isEmpty || res.getDecisionResults.getFirst.hasErrors || res.getDecisionResults.getFirst.getResult == null)
+    if (res.hasErrors || res.getDecisionResults.isEmpty || res.getDecisionResults.asScala.head.hasErrors || res.getDecisionResults.asScala.head.getResult == null)
       null
     else
-      new GenericArrayData(res.getDecisionResults.getFirst.getResult.asInstanceOf[util.ArrayList[Boolean]].toArray)
+      new GenericArrayData(res.getDecisionResults.asScala.head.getResult.asInstanceOf[util.ArrayList[Boolean]].toArray)
   }
 
   override def eval(input: InternalRow): Any = ???
